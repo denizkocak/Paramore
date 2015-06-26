47543ff051d16d00d496b39b8521bcbea702e778
deniz.kocak <deniz.kocak@huddle.com>
2015-06-26 10:03:27 +0100

    greeting service updated to support sqs

---
+++ Brighter/paramore.brighter.commandprocessor.messaginggateway.awssqs/InputChannelFactory.cs
@@ -0,0 +1,38 @@
+﻿namespace paramore.brighter.commandprocessor.messaginggateway.awssqs
+{
+    public class InputChannelFactory : IAmAChannelFactory
+    {
+        private readonly SqsMessageConsumerFactory _messageConsumerFactory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputChannelFactory"/> class.
+        /// </summary>
+        /// <param name="messageConsumerFactory">The messageConsumerFactory.</param>
+        public InputChannelFactory(SqsMessageConsumerFactory messageConsumerFactory)
+        {
+            _messageConsumerFactory = messageConsumerFactory;
+        }
+
+        /// <summary>
+        /// Creates the input channel.
+        /// </summary>
+        /// <param name="channelName">Name of the channel.</param>
+        /// <param name="routingKey">The routing key.</param>
+        /// <returns>IAmAnInputChannel.</returns>
+        public IAmAnInputChannel CreateInputChannel(string channelName, string routingKey)
+        {
+            return new InputChannel(channelName, _messageConsumerFactory.Create(channelName, routingKey));
+        }
+
+        /// <summary>
+        /// Creates the output channel.
+        /// </summary>
+        /// <param name="channelName">Name of the channel.</param>
+        /// <param name="routingKey">The routing key.</param>
+        /// <returns>IAmAnInputChannel.</returns>
+        public IAmAnInputChannel CreateOutputChannel(string channelName, string routingKey)
+        {
+            return new InputChannel(channelName, _messageConsumerFactory.Create(channelName, routingKey));
+        }
+    }
+}
